bf1daef75cd193b5b7ab8993432c784320143bae
shift-row-RL <182500980+shift-row-RL@users.noreply.github.com>
2024-11-05 22:21:11 +0100

    setup linting for prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,15 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-
+import eslintConfigPrettier from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {files: ["**/*.{js,mjs,cjs,ts}"]},
   {languageOptions: { globals: globals.browser }},
+  {rules:{...prettierPlugin.configs.rules}},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  eslintConfigPrettier,
 ];
