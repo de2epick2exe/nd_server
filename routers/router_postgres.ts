1f7d429f3130faabca8ebcc8eb827ba70f775add
de2epick2exe <42617421+de2epick2exe@users.noreply.github.com>
2024-11-08 13:09:27 +0100

    prettier

--- routers/router_postgres.ts
+++ routers/router_postgres.ts
@@ -6,15 +6,17 @@ export const pg_router = new Router();
 // GET
 pg_router.get("/artists", pg_controller.get_artists);
 pg_router.get("/artist/:id", pg_controller.get_artist);
+
 //POST
 pg_router.post("/create_artist", async (ctx: Context) => {
   ctx.body = "200";
 });
+
 //PUT
 pg_router.put("/update_Album");
 
 //PATCH
-pg_router.patch("/update_Album")
+pg_router.patch("/update_Album");
 
 //DELETE
 pg_router.delete("/artist/:id", pg_controller.delete_artists);
