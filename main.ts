a413454ddb23ad36591614d3670b11b754898c01
de2epick2exe <42617421+de2epick2exe@users.noreply.github.com>
2024-11-08 12:22:58 +0100

    git init & env template

--- main.ts
+++ main.ts
@@ -3,10 +3,8 @@ import { router } from "./router";
 
 const app = new Koa();
 
-app.use(router.routes())
+app.use(router.routes());
 
 app.listen(8080, () => {
   console.log(`🚀 Server is running on port http://localhost:8080/`);
 });
-
-
